bfd91925129345698420ede450f382767d9372b7
pgogousis <pgogousis@gmail.com>
2018-10-24 22:33:40 +0100

    Change run script and execute all input files

--- Task_1/run1.sh
+++ Task_1/run1.sh
@@ -2,7 +2,7 @@ hadoop jar /opt/hadoop/hadoop-2.9.1/share/hadoop/tools/lib/hadoop-streaming-2.9.
 	-D mapred.job.name="gogopavl-word-line" \
 	-D mapred.reduce.tasks=1 \
 	-input /data/small/gutenberg/8830902613096153013.txt \
-	-output /user/s1884197/data/output/task1 \
+	-output /user/s1884197/data/output/task1/out \
 	-mapper mapper1.py \
 	-reducer reducer1.py \
 	-file mapper1.py \
