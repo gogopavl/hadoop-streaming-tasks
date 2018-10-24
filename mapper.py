e8d571ae74ebcbf7c74fc700b4bebdd4ba35320c
Pavlos Gogousis <pgogousis@gmail.com>
2018-10-24 19:17:18 +0100

    Replacing mapper with memory efficient version

--- mapper.py
+++ mapper.py
@@ -2,13 +2,29 @@
 # mapper.py
 import sys
 
+from collections import defaultdict
+
+word_dict = defaultdict(int)
+MAX_SIZE = 100
+
 def map_function(document):
-    # Split document by words and use word as the key
+    # Split document by words where word is the key
     for word in document.strip().split():
         yield word, 1
 
 for line in sys.stdin:
-    # Call map_function for each line in the input
+    # Call the map_function for each line in the input
     for key, value in map_function(line):
-        # Emit key-value pairs using '\t' as a delimeter
-        print(key + "\t" + str(value))
+        # Agregate value for a word locally
+        word_dict[key] += value
+
+        # To keep O(1) space, we bound the size of our memory footprint
+        if len(word_dict) > MAX_SIZE:
+            for key, value in word_dict.items():
+                print(key + "\t" + str(value))
+
+            word_dict.clear()
+
+# Emit leftover key-value pairs and use '\t' as the delimiter
+for key, value in word_dict.items():
+    print(key + "\t" + str(value))
--- reducer.py
+++ reducer.py
@@ -5,8 +5,8 @@ import sys
 prev_word = None
 value_total = 0
 
-for line in sys.stdin: # For ever line in the input from stdin
-    line = line.strip() # Remove trailing characters
+for line in sys.stdin:          # For ever line in the input from stdin
+    line = line.strip()         # Remove trailing characters
     word, value = line.split("\t", 1)
     value = int(value)
 
@@ -14,11 +14,11 @@ for line in sys.stdin: # For ever line in the input from stdin
     if prev_word == word:
         value_total += value
     else:
-        if prev_word != None: # Write result to stdout
+        if prev_word != None:  # Write result to stdout
             print("{0}\t{1}".format(prev_word, value_total))
 
         value_total = value
         prev_word = word
 
-if prev_word == word: # Don't forget the last key/value pair
+if prev_word == word:  # Don't forget the last key/value pair
     print("{0}\t{1}".format(prev_word, value_total))
