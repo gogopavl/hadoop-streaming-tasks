b23853ff709da8163cfb5f53021ef10e15316e17
pgogousis <pgogousis@gmail.com>
2018-10-24 22:10:15 +0100

    Adding Task 1 solution // Check combiners

---
+++ Task_1/run1.sh
@@ -0,0 +1,9 @@
+hadoop jar /opt/hadoop/hadoop-2.9.1/share/hadoop/tools/lib/hadoop-streaming-2.9.1.jar \
+	-D mapred.job.name="gogopavl-word-line" \
+	-D mapred.reduce.tasks=1 \
+	-input /data/small/gutenberg/8830902613096153013.txt \
+	-output /user/s1884197/data/output/task1 \
+	-mapper mapper1.py \
+	-reducer reducer1.py \
+	-file mapper1.py \
+	-file reducer1.py
