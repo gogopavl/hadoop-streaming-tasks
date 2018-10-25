bc1b3d7df4feb51b240c6f7120eb906de21a5356
pgogousis <pgogousis@gmail.com>
2018-10-26 00:37:05 +0100

    Adding Task 6 // Efficiency and precision - mappers calculate partial average. Should Reducer calculate them all?

---
+++ Task_6/mapper6.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python2
+# mapper3.py
+import sys
+from collections import defaultdict
+
+
+totalVotes = 0
+totalNum = 0
+
+def map_function(line):
+    votes = float(line.split("\t")[2].strip())
+    # check non float?
+    yield votes, 1
+
+for line in sys.stdin:
+    # Call the map_function for each line in the input
+    for key, value in map_function(line):
+        # Agregate value for a word locally
+        totalVotes += key
+        totalNum += value
+
+average = totalVotes / totalNum
+print(str(average))
