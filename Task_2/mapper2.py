cbe9a4d92e5fc991e91c71d3e7181c0a691f4c9d
pgogousis <pgogousis@gmail.com>
2018-10-26 02:35:16 +0100

    Fixed bug in Task 2

--- Task_2/mapper2.py
+++ Task_2/mapper2.py
@@ -5,21 +5,9 @@ import sys
 from collections import defaultdict
 
 bigramDictionary = defaultdict(int) # Structure to temporarily save bigram terms
-MAX_SIZE = 100
+MAX_SIZE = 500
 
 def getBigrams(inputList):
-    """Calculates the bigrams formed from given words
-
-    Parameters
-    ----------
-    inputList : List of Strings
-        List of given words
-
-    Returns
-    -------
-    bigramList : List of tuples
-        A list containing all bigrams formed out the given words
-    """
     return zip(inputList, inputList[1:]) # zip returns all tuple pairs from given inputs
 
 def map_function(line):
@@ -35,13 +23,11 @@ for line in sys.stdin:
         bigramDictionary[key] += value
 
         # To keep O(1) space, we bound the size of our memory footprint
-        if len(bigramDictionary) > MAX_SIZE:
+        if len(bigramDictionary) >= MAX_SIZE:
             for key, value in bigramDictionary.items():
-                if value > 5: # Write bigrams with frequency greater than 5
-                    print(key + "\t" + str(value))
+                print(key + "\t" + str(value))
             bigramDictionary.clear()
 
 # Emit leftover key-value pairs and use '\t' as the delimiter
 for key, value in bigramDictionary.items():
-    if value > 5: # Write bigrams with frequency greater than 5
-        print(key + "\t" + str(value))
+    print(key + "\t" + str(value))
